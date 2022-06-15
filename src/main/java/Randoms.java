fc710449e2ab49427e8087fdff06aad8c506e003
Алексей Печин <organner@mail.ru>
2022-06-15 21:31:04 +0300

    Creating class Randoms.java

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Randoms {
+    protected Random random;
+    protected int min;
+    protected int max;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+}
